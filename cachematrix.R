8b73dd99c4a0f27c38c12c62e49e72d72b91fbb9
eisenjulian <julianeisen@julianeisen-mbp.local>
2014-06-02 22:12:26 -0700

    Assignment2
    This is the first attempt for the assignment2 in the R programming
    course. Implements makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two function provide a wrapper around a
+## a matrix to cache the calculation of the inverse
 
-## Write a short comment describing this function
+## This function creates the wrapper object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+                setinverse = setinverse,
+                getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the wrapped matrix
+## performing a lazy evaluation and saving the inverse computation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
